e396f240364a093310b816913c1a8edd67979618
holosam <83981798+holosam@users.noreply.github.com>
2024-04-04 10:43:16 -0600

    Simplify simulation code with flags, update configs, update to go122 (#1)
    * changes for building image
    
    * update configs
    
    * update configs
    
    ---------
    
    Co-authored-by: Sammy Farid <sammy@sammys-mbp.lan>

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -5,56 +5,56 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
-// Switch to https://github.com/spf13/cobra.
+var (
+	envThreads = "SIM_THREADS"
+	// Type of traffic to simulate, options: [ max, cyclical, bursty ]
+	envTrafficType = "SIM_TRAFFIC_TYPE"
+)
+
+var (
+	feedURL     = flag.String("url", "", "Base url for the web app.")
+	simDuration = flag.Duration("d", 30*time.Minute, "How long to run for.")
+)
+
 func main() {
-	simulateCmd := flag.NewFlagSet("simulate", flag.ExitOnError)
-	simFeedURL := simulateCmd.String("url", "", "Base url for the web app.")
-	simDuration := simulateCmd.Duration("d", 30*time.Minute, "How long to run for.")
-	simConcurrency := simulateCmd.Int("c", 3, "Max threads to use.")
-	simUsers := simulateCmd.Int("u", 100, "Number of users to hit.")
-	simType := simulateCmd.String("t", "cyclical", "Type of traffic to simulate, options: [ max, cyclical, bursty ]")
+	flag.Parse()
 
-	if len(os.Args) < 2 {
-		log.Fatal("Need a subcommand.")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if *feedURL == "" {
+		log.Fatalf("No input URL to hit.")
 	}
 
-	switch os.Args[1] {
-	case simulateCmd.Name():
-		simulateCmd.Parse(os.Args[2:])
+	threadsStr := os.Getenv(envThreads)
+	threads, err := strconv.Atoi(threadsStr)
+	if err != nil {
+		threads = 5
+	}
 
-		ctx, cancel := context.WithTimeout(context.Background(), *simDuration)
-		defer cancel()
+	sim := NewSimulation(SimParams{
+		BaseURL:      *feedURL,
+		Concurrency:  threads,
+		MaxUserIndex: 100,
+		Length:       *simDuration,
+	})
 
-		if *simFeedURL == "" {
-			log.Fatalf("No input URL to hit.")
-		}
-
-		sim := NewSimulation(SimParams{
-			BaseURL:      *simFeedURL,
-			Concurrency:  *simConcurrency,
-			MaxUserIndex: *simUsers,
-			Length:       *simDuration,
-		})
-
-		switch *simType {
-		case "max":
-			sim.RunFlat(ctx)
-		case "cyclical":
-			sim.RunCyclical(ctx)
-		case "bursty":
-			sim.RunBursty(ctx)
-		default:
-			log.Fatalf("Unrecognized type: %s", *simType)
-		}
-
-		sim.PrintStats()
-
+	switch os.Getenv(envTrafficType) {
+	case "cyclical":
+		sim.RunCyclical(ctx)
+	case "bursty":
+		sim.RunBursty(ctx)
+	case "max":
+		fallthrough
 	default:
-		log.Fatalf("Unrecognized command: %s", os.Args[1])
+		sim.RunFlat(ctx)
 	}
+
+	sim.PrintStats()
 
 	log.Printf("Exited successfully")
 }
